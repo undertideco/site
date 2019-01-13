bf275ddb055ac003984de2fb00ae10972f152d49
Duncan Leo <duncanleo97@gmail.com>
2019-01-13 22:34:44 +0800

    add typing animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
+import Typed from 'typed.js';
+
 import '../scss/index.scss';
+
+const options = {
+  strings: ['reality.', 'success.', 'delight.'],
+  typeSpeed: 40,
+  loop: true,
+  backDelay: 3000,
+  cursorChar: '_',
+  backSpeed: 80,
+  contentType: null,
+};
+
+const typed = new Typed('.container__title__anim', options);
